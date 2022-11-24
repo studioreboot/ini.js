ea22ed6c63f4cf04e990b5e0fa771db0f19f4fee
StudioReboot <72804242+studioreboot@users.noreply.github.com>
2022-11-23 19:56:35 -0500

    Create ini.js

---
+++ ini.js
@@ -0,0 +1,49 @@
+var INI = {
+    parse: function (stream) {
+        var ini = {}, ptr = ini, lines = stream.split("\n");
+
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+            if (line.startsWith("[") && line.endsWith("]")) {
+                ptr = {};
+                ini[line.substring(1, ini.length - 1)] = ptr;
+            } else if (line.indexOf("=") != -1 && !line.startsWith(";")) {
+                var name = line.substring(0, line.indexOf("=")).trim(),
+                    value = line.substring(line.indexOf("=") + 1, line.length),
+                    trimmed = value.trim();
+                if (/true|false/ig.test(trimmed)) {
+                    value = trimmed.toLowerCase() === "true";
+                } else if (+trimmed === parseFloat(trimmed)) {
+                    value = +trimmed;
+                }
+                ptr[name] = value;
+            }
+        }
+
+        return ini;
+    },
+    generate: function (object) {
+        var result = "";
+
+        var keys = Object.keys(object);
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i], value = object[key];
+            if (typeof value == "object") {
+                result += "\n[" + key + "]\n";
+                const tkeys = Object.keys(value);
+                for (let j = 0; j < tkeys.length; j++) {
+                    const tkey = tkeys[j];
+                    result += tkey.toString() + " = " + value[tkey].toString() + "\n";
+                }
+            } else {
+                result += key + " = " + object[key].toString() + "\n";
+            }
+        }
+
+        return result;
+    }
+}
+
+if (module) {
+    module.exports = INI;
+}
