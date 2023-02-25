0fb25eba74c6c71098a079f0a971d9278d41970d
StudioReboot <72804242+studioreboot@users.noreply.github.com>
2023-02-25 15:10:09 -0500

    changed INI.js

--- ini.js
+++ ini.js
@@ -1,46 +1,84 @@
 var INI = {
-    parse: function (stream) {
-        var ini = {}, ptr = ini, lines = stream.split("\n");
+    /** @param {string} string */
+    parse: function (string) {
+        var lines = string.split("\n");
+        var ini = {}, ptr = ini;
 
         for (let i = 0; i < lines.length; i++) {
-            const line = lines[i];
-            if (line.trim().startsWith("[") && line.trim().endsWith("]")) {
+            const line = INI.removeComment(lines[i]);
+
+            if (line.length < 2) continue;
+
+            if (line.charAt(0) === "[" && line.charAt(line.length - 1) === "]") {
                 ptr = {};
                 ini[line.substring(1, line.length - 1)] = ptr;
-            } else if (line.indexOf("=") != -1 && !line.startsWith(";")) {
-                var name = line.substring(0, line.indexOf("=")).trim(),
-                    value = line.substring(line.indexOf("=") + 1, line.length),
-                    trimmed = value.trim();
-                if (/true|false/ig.test(trimmed)) {
-                    value = trimmed.toLowerCase() === "true";
-                } else if (+trimmed == parseFloat(trimmed)) {
-                    value = +trimmed;
-                }
-                ptr[name] = value;
+            } else {
+                var key = line.substring(0, line.indexOf("="));
+                var value = INI.parseValue(line.substring(line.indexOf("=") + 1, line.length));
+
+                ptr[key] = value;
             }
+            
         }
 
         return ini;
     },
-    generate: function (object) {
-        var result = "";
+    /** @param {string} string */
+    removeComment: function (string) {
+        if (string.indexOf(";") != -1) {
+            return string.substring(0, string.indexOf(";")).trim();
+        }
+        return string;
+    },
+    /** @param {string} string */
+    parseValue: function (thing) {
+        var trimmed = thing.trim();
 
-        var keys = Object.keys(object);
-        for (let i = 0; i < keys.length; i++) {
-            const key = keys[i], value = object[key];
-            if (typeof value == "object") {
-                result += "\n[" + key + "]\n";
-                const tkeys = Object.keys(value);
-                for (let j = 0; j < tkeys.length; j++) {
-                    const tkey = tkeys[j];
-                    result += tkey.toString() + " = " + value[tkey].toString() + "\n";
+        if (!/\D/gi.test(trimmed) || trimmed === "Infinity" || trimmed === "NaN") {
+            return +thing;
+        }
+
+        if (/true|false/ig.test(trimmed)) {
+            return trimmed === "true";
+        }
+
+        return thing;
+    },
+    /** @param {Object} anObject */
+    build: function (anObject) {
+        var out = "", mainProps, sectionedProps, props;
+
+        mainProps = Object.keys(anObject).filter(key => {
+            return typeof anObject[key] !== "object";
+        });
+        
+        sectionedProps = Object.keys(anObject).filter(key => typeof anObject[key] === "object");
+
+        for (let i = 0; i < mainProps.length; i++) {
+            const prop = mainProps[i];
+
+            out += prop + "=" + anObject[prop] + "\n";
+        };
+
+        for (let i = 0; i < sectionedProps.length; i++) {
+            const section = sectionedProps[i];
+            out += "\n[" + section + "]\n"
+
+            props = Object.keys(anObject[section]);
+
+            for (let j = 0; j < props.length; j++) {
+                const prop = props[j];
+
+                var value = anObject[section][prop];
+                if (typeof value === "object") {
+                    throw new Error("INI files cannot have multi-layered sections!");
                 }
-            } else {
-                result += key + " = " + object[key].toString() + "\n";
+
+                out += prop + "=" + value + "\n";
             }
         }
 
-        return result;
+        return out;
     }
 }
 
